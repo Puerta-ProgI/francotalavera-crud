8ffd6ace209745ab19bb9315762f9306254eec81
Lukas <usuario@PUERTA18.CORP.AR>
2023-11-03 12:30:28 -0300

    first commit

---
+++ src/controllers/main-controller.js
@@ -0,0 +1,25 @@
+const products=require("../services/products-services") 
+
+let baseUrl = "http://localhost:3031"
+let url = '/api'
+
+const controller = {
+    home: (req, res) => {
+        res.redirect(url);
+    },
+    apiHome: (req,res) => {
+        res.send({
+            productos: baseUrl + url + "/productos",
+        });
+    },
+    apiProducts: (req,res) => {
+        let panes = products.listarPanes()
+        res.send(panes);
+    },
+    apiProductDetail: (req,res) => {
+        const id = req.params.id;
+        let panAEnviar = products.buscarPan(id)
+        res.send(panAEnviar || {});
+    },
+}
+module.exports = controller;
